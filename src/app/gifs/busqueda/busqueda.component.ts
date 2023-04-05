d3f5f0f6299e79237628330ebf6c4ef4a4eb5465
Juan Camilo Céspedes Romero <juankamilocromero@gmail.com>
2023-04-04 21:49:34 -0500

    ultima actualizacion capitulo 86

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent {
+
+  buscar(event:KeyboardEvent){
+    console.log("HEttttt")
+  }
+}
